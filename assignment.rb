e499d02936530991b171a522edc3b82e9d100490
amit patel <amitkumarpatel44@gmail.com>
2019-04-28 17:39:46 +0530

    added function to the code and test cases

--- assignment.rb
+++ assignment.rb
@@ -24,8 +24,23 @@ def load_words_dictionary(characters_arr)
 	return loaded_words_3chars, loaded_words_4chars, loaded_words_5chars, loaded_words_6chars, loaded_words_7chars, loaded_words_10chars
 end
 
+# generate combination of numbers from the entered phone number
+def get_combination_of_number(numbers, combination_of_split, int_mapping_char)
+	combination_of_numbers, first_characters_of_words  = [], []
+	combination_of_split.each do |elements|
+		index = 0
+		elements.each do |element|
+			combination_of_numbers << numbers[index..(index+element - 1)]
+			first_characters_of_words << int_mapping_char[numbers[index].to_i]
+			index += element
+		end
+	end
+	return combination_of_numbers, first_characters_of_words
+end
+
 #function joins words to generate the desired output
-def make_words(words, str)
+def make_words(str)
+	words = []
 	if !str.compact[0].nil? && !str.compact[1].nil?
 		str[0].compact.each do |ele1|
 			str.compact[1].each do |ele2|
@@ -36,95 +51,72 @@ def make_words(words, str)
 	words
 end
 
-puts "Please Enter 10 digit number except 0 and 1"
-digits = gets.strip
-
-if digits.size != 10 || digits.to_i.to_s != digits || digits.include?('0') ||  digits.include?('1')
-	puts "This is not a valid phone number. Please enter a valid phone number"
-else
-	int_mapping_char = {2 => ['a', 'b', 'c'], 3 => ['d', 'e', 'f'], 4 => ['g', 'h', 'i'], 5 => ['j', 'k', 'l'], 6 => ['m', 'n', 'o'], 7 => ['p', 'q', 'r', 's'], 8 => ['t', 'u', 'v'], 9 => ['w', 'x', 'y', 'z']}
-	combination_of_split = [[3,3,4], [3,4,3],[4,3,3],[3,7],[7,3],[4,6],[6,4],[5,5],[10]]
+# function to loop in all the combinations of digits and returns the words from dictionary
+def combination_of_words(combination_of_digits, first_characters_of_word, int_mapping_char)
 	words = []
-	combination_of_digits = []
-	first_characters_of_word = []
-
-	combination_of_split.each do |elements|
-		index = 0
-		elements.each do |ele|
-			combination_of_digits << digits[index..(index+ele - 1)]
-			first_characters_of_word << int_mapping_char[digits[index].to_i]
-			index += ele
-		end
-	end
-
 	first_str, third_str, fifth_str, sixth_str = [], [], [], []
 	loaded_words_3chars, loaded_words_4chars, loaded_words_5chars, loaded_words_6chars, loaded_words_7chars, loaded_words_10chars = load_words_dictionary(first_characters_of_word.uniq)
-
-		combination_of_digits.each_with_index do |combi, index|
-			if  combi.length == 3
-				loaded_words_3chars.each do |word|
-					if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)
-						if index == 0 || index == 1 || index == 3 || index == 5 || index == 7 || index == 8
-							first_str[index] = [] if first_str[index].nil?
-							first_str[index] << word
-						end
-						 if index == 9 || index == 12
-							third_str[index] = [] if third_str[index].nil?
-							third_str[index] << word
-						end
+	combination_of_digits.each_with_index do |combi, index|
+		if  combi.length == 3
+			loaded_words_3chars.each do |word|
+				if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)
+					if index == 0 || index == 1 || index == 3 || index == 5 || index == 7 || index == 8
+						first_str[index] = [] if first_str[index].nil?
+						first_str[index] << word
 					end
-				end
-			end 
-
-			if combi.length == 4
-				loaded_words_4chars.each do |word|
-					if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase) && int_mapping_char[combi[3].to_i].include?(word[3].downcase)
-						if index == 2 || index == 4 || index == 6
-							first_str[index] = [] if first_str[index].nil?
-							first_str[index] << word
-						end
-						if index == 13 || index == 16	
-							fifth_str[index] = [] if fifth_str[index].nil?
-							fifth_str[index] << word
-						end
-					end
-				end
-			end
-
-			if combi.length == 7
-				loaded_words_7chars.each do |word|
-					if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase) && int_mapping_char[combi[5].to_i].include?(word[5].downcase) && int_mapping_char[combi[6].to_i].include?(word[6].downcase)
+					 if index == 9 || index == 12
 						third_str[index] = [] if third_str[index].nil?
 						third_str[index] << word
 					end
 				end
 			end
-
-			if combi.length == 5
-				loaded_words_5chars.each do |word|
-					if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase)
-						sixth_str[index] = [] if sixth_str[index].nil?
-						sixth_str[index] << word
+		end
+		if combi.length == 4
+			loaded_words_4chars.each do |word|
+				if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase) && int_mapping_char[combi[3].to_i].include?(word[3].downcase)
+					if index == 2 || index == 4 || index == 6
+						first_str[index] = [] if first_str[index].nil?
+						first_str[index] << word
 					end
-				end
-			end
-			if combi.length == 6
-				loaded_words_6chars.each do |word|
-					if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase) && int_mapping_char[combi[5].to_i].include?(word[5].downcase)
+					if index == 13 || index == 16	
 						fifth_str[index] = [] if fifth_str[index].nil?
 						fifth_str[index] << word
 					end
 				end
 			end
-			if combi.length == 10
-				loaded_words_10chars.each do |word|
-					if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase) && int_mapping_char[combi[5].to_i].include?(word[5].downcase) && int_mapping_char[combi[6].to_i].include?(word[6].downcase) && int_mapping_char[combi[7].to_i].include?(word[7].downcase) && int_mapping_char[combi[8].to_i].include?(word[8].downcase) && int_mapping_char[combi[9].to_i].include?(word[9].downcase)
-						words << word
-					end
+		end
+		if combi.length == 7
+			loaded_words_7chars.each do |word|
+				if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase) && int_mapping_char[combi[5].to_i].include?(word[5].downcase) && int_mapping_char[combi[6].to_i].include?(word[6].downcase)
+					third_str[index] = [] if third_str[index].nil?
+					third_str[index] << word
 				end
 			end
 		end
-
+		if combi.length == 5
+			loaded_words_5chars.each do |word|
+				if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase)
+					sixth_str[index] = [] if sixth_str[index].nil?
+					sixth_str[index] << word
+				end
+			end
+		end
+		if combi.length == 6
+			loaded_words_6chars.each do |word|
+				if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase) && int_mapping_char[combi[5].to_i].include?(word[5].downcase)
+					fifth_str[index] = [] if fifth_str[index].nil?
+					fifth_str[index] << word
+				end
+			end
+		end
+		if combi.length == 10
+			loaded_words_10chars.each do |word|
+				if int_mapping_char[combi[0].to_i].include?(word[0].downcase) && int_mapping_char[combi[1].to_i].include?(word[1].downcase)  && int_mapping_char[combi[2].to_i].include?(word[2].downcase)  && int_mapping_char[combi[3].to_i].include?(word[3].downcase) && int_mapping_char[combi[4].to_i].include?(word[4].downcase) && int_mapping_char[combi[5].to_i].include?(word[5].downcase) && int_mapping_char[combi[6].to_i].include?(word[6].downcase) && int_mapping_char[combi[7].to_i].include?(word[7].downcase) && int_mapping_char[combi[8].to_i].include?(word[8].downcase) && int_mapping_char[combi[9].to_i].include?(word[9].downcase)
+					words << word
+				end
+			end
+		end
+	end
 	if !first_str[0].nil? && !first_str.compact[1].nil? && !first_str.compact[2].nil?
 		first_str[0].each do |ele1|
 			first_str.compact[1].each do |ele2|
@@ -134,10 +126,22 @@ else
 			end
 		end
 	end
+	words << make_words(third_str.compact)
+	words << make_words(fifth_str.compact)
+	words << make_words(sixth_str.compact)
+	words
+end
 
-	words = make_words(words, third_str.compact)
-	words = make_words(words, fifth_str.compact)
-	words = make_words(words, sixth_str.compact)
-
-	print words.uniq
+puts "Please Enter 10 digit number except 0 and 1"
+phone_number_string = gets.strip
+if phone_number_string.size != 10 || phone_number_string.to_i.to_s != phone_number_string || phone_number_string.include?('0') ||  phone_number_string.include?('1')
+	puts "This is not a valid phone number. Please enter a valid phone number"
+else
+	output_words = []
+	int_mapping_char = {2 => ['a', 'b', 'c'], 3 => ['d', 'e', 'f'], 4 => ['g', 'h', 'i'], 5 => ['j', 'k', 'l'], 6 => ['m', 'n', 'o'], 7 => ['p', 'q', 'r', 's'], 8 => ['t', 'u', 'v'], 9 => ['w', 'x', 'y', 'z']}
+	combination_of_split = [[3,3,4], [3,4,3],[4,3,3],[3,7],[7,3],[4,6],[6,4],[5,5],[10]]
+	
+	combination_of_digits, first_characters_of_words = get_combination_of_number(phone_number_string, combination_of_split, int_mapping_char)
+	output_words = combination_of_words(combination_of_digits, first_characters_of_words, int_mapping_char)
+	print output_words.flatten.uniq
 end
--- spec/assignment_spec.rb
+++ spec/assignment_spec.rb
@@ -21,12 +21,41 @@ describe 'assignment' do
 	  end
 	end
 
-	# context "should provide desired output" do
-	# 	it "should contain 10 digits" do
-	#     expect(capture_phonenumber('6686787825')).to be 
-	#   end
-	# end
+	context "should provide combination of digits with splits" do
+    int_mapping_char = {2 => ['a', 'b', 'c'], 3 => ['d', 'e', 'f'], 4 => ['g', 'h', 'i'], 5 => ['j', 'k', 'l'], 6 => ['m', 'n', 'o'], 7 => ['p', 'q', 'r', 's'], 8 => ['t', 'u', 'v'], 9 => ['w', 'x', 'y', 'z']}
+		combination_of_split = [[3,3,4], [3,4,3],[4,3,3],[3,7],[7,3],[4,6],[6,4],[5,5],[10]] 
 
+		it "should return integer combination and letter array" do
+	    phone_number_string = capture_phonenumber('6686787825')
+	    combination_of_digits, first_characters_of_words = get_combination_of_number(phone_number_string, combination_of_split, int_mapping_char)
+      expect(combination_of_digits).to eq(["668", "678", "7825", "668", "6787", "825", "6686", "787", "825", "668", "6787825", "6686787", "825", "6686", "787825", "668678", "7825", "66867", "87825", "6686787825"])
+			expect(first_characters_of_words).to eq([["m", "n", "o"], ["m", "n", "o"], ["p", "q", "r", "s"], ["m", "n", "o"], ["m", "n", "o"], ["t", "u", "v"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["m", "n", "o"], ["m", "n", "o"], ["m", "n", "o"], ["t", "u", "v"], ["m", "n", "o"], ["p", "q", "r", "s"], ["m", "n", "o"], ["p", "q", "r", "s"], ["m", "n", "o"], ["t", "u", "v"], ["m", "n", "o"]])
+	  end
 
+		it "should print output string combination" do
+	    phone_number_string = capture_phonenumber('2282668687')
+	    combination_of_digits, first_characters_of_words = get_combination_of_number(phone_number_string, combination_of_split, int_mapping_char)
+      expect(combination_of_digits).to eq(["228", "266", "8687", "228", "2668", "687", "2282", "668", "687", "228", "2668687", "2282668", "687", "2282", "668687", "228266", "8687", "22826", "68687", "2282668687"])
+			expect(first_characters_of_words).to eq([["a", "b", "c"], ["a", "b", "c"], ["t", "u", "v"], ["a", "b", "c"], ["a", "b", "c"], ["m", "n", "o"], ["a", "b", "c"], ["m", "n", "o"], ["m", "n", "o"], ["a", "b", "c"], ["a", "b", "c"], ["a", "b", "c"], ["m", "n", "o"], ["a", "b", "c"], ["m", "n", "o"], ["a", "b", "c"], ["t", "u", "v"], ["a", "b", "c"], ["m", "n", "o"], ["a", "b", "c"]])
+	  end
+	end
 
+	context "should provide desired output for phone numbers" do
+    int_mapping_char = {2 => ['a', 'b', 'c'], 3 => ['d', 'e', 'f'], 4 => ['g', 'h', 'i'], 5 => ['j', 'k', 'l'], 6 => ['m', 'n', 'o'], 7 => ['p', 'q', 'r', 's'], 8 => ['t', 'u', 'v'], 9 => ['w', 'x', 'y', 'z']}
+		combination_of_split = [[3,3,4], [3,4,3],[4,3,3],[3,7],[7,3],[4,6],[6,4],[5,5],[10]] 
+
+		it "should print string combination" do
+	    phone_number_string = capture_phonenumber('6686787825')
+	    combination_of_digits, first_characters_of_words = get_combination_of_number(phone_number_string, combination_of_split, int_mapping_char)
+      result = combination_of_words(combination_of_digits, first_characters_of_words, int_mapping_char)
+      expect(result.flatten).to eq(["MOTORTRUCK", "MOT,OPT,PUCK", "MOT,OPT,RUCK", "MOT,OPT,SUCK", "MOT,ORT,PUCK", "MOT,ORT,RUCK", "MOT,ORT,SUCK", "NOT,OPT,PUCK", "NOT,OPT,RUCK", "NOT,OPT,SUCK", "NOT,ORT,PUCK", "NOT,ORT,RUCK", "NOT,ORT,SUCK", "OOT,OPT,PUCK", "OOT,OPT,RUCK", "OOT,OPT,SUCK", "OOT,ORT,PUCK", "OOT,ORT,RUCK", "OOT,ORT,SUCK", "MOT,TAJ", "NOT,TAJ", "OOT,TAJ", "NOUN,STRUCK", "ONTO,STRUCK", "MOTOR,TRUCK", "MOTOR,USUAL", "NOUNS,TRUCK", "NOUNS,USUAL"])
+	  end
+
+		it "should print output string combination" do
+	    phone_number_string = capture_phonenumber('2282668687')
+	    combination_of_digits, first_characters_of_words = get_combination_of_number(phone_number_string, combination_of_split, int_mapping_char)
+      result = combination_of_words(combination_of_digits, first_characters_of_words, int_mapping_char)
+      expect(result.flatten).to eq(["CATAMOUNTS", "ACT,BOO,TOTS", "ACT,BOO,TOUR", "ACT,CON,TOTS", "ACT,CON,TOUR", "ACT,COO,TOTS", "ACT,COO,TOUR", "BAT,BOO,TOTS", "BAT,BOO,TOUR", "BAT,CON,TOTS", "BAT,CON,TOUR", "BAT,COO,TOTS", "BAT,COO,TOUR", "CAT,BOO,TOTS", "CAT,BOO,TOUR", "CAT,CON,TOTS", "CAT,CON,TOUR", "CAT,COO,TOTS", "CAT,COO,TOUR", "ACT,AMOUNTS", "ACT,CONTOUR", "BAT,AMOUNTS", "BAT,CONTOUR", "CAT,AMOUNTS", "CAT,CONTOUR", "ACTA,MOUNTS"])
+	  end
+	end
 end
